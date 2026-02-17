14e7e76a15e097662d002d14a69e99f53e2aaacf
s91k <s91k@hotmail.com>
2026-02-17 12:59:02 +0100

    add end-point for creating a new booking (#5)

--- src/main/java/se/grupp3/hotellbokningssystem/service/BookingService.java
+++ src/main/java/se/grupp3/hotellbokningssystem/service/BookingService.java
@@ -1,10 +1,13 @@
 package se.grupp3.hotellbokningssystem.service;
 
 import org.springframework.stereotype.Service;
+import se.grupp3.hotellbokningssystem.exception.OutOfRoomsException;
 import se.grupp3.hotellbokningssystem.model.Booking;
+import se.grupp3.hotellbokningssystem.model.BookingStatus;
+import se.grupp3.hotellbokningssystem.model.RoomType;
 import se.grupp3.hotellbokningssystem.repository.BookingRepository;
 
-import java.util.List;
+import java.util.Collection;
 
 @Service
 public class BookingService {
@@ -15,7 +18,23 @@ public class BookingService {
         this.bookingRepository = bookingRepository;
     }
 
-    public List<Booking> getBookings(){
+    public Collection<Booking> getBookings(){
         return this.bookingRepository.getBookings();
+    }
+
+    public Booking createBooking(String guestName, Integer nights, Integer guestCount, RoomType roomType) throws IllegalArgumentException, OutOfRoomsException {
+        if(this.bookingRepository.getNrOfBookedRooms(roomType) >= roomType.getNrOfRooms()){
+            throw new OutOfRoomsException(roomType);
+        }
+
+        if(guestCount > roomType.getMaxGuests()){
+            throw new IllegalArgumentException("Too many guests for the selected room type");
+        }
+
+        Booking b = new Booking(null, guestName, guestCount, roomType, nights, roomType.getPricePerNight() * nights, BookingStatus.CONFIRMED);
+
+        bookingRepository.addBooking(b);
+
+        return b;
     }
 }
