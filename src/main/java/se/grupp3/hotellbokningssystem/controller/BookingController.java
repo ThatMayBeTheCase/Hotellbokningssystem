2625ef8d7fd307436b6e060af69f1f0153d03297
Simon <s91k@hotmail.com>
2026-02-19 03:49:48 +0100

    only allow users to see their own bookings

--- src/main/java/se/grupp3/hotellbokningssystem/controller/BookingController.java
+++ src/main/java/se/grupp3/hotellbokningssystem/controller/BookingController.java
@@ -4,13 +4,14 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import se.grupp3.hotellbokningssystem.dto.BookingRequest;
 import se.grupp3.hotellbokningssystem.dto.BookingResponse;
-import se.grupp3.hotellbokningssystem.exception.OutOfRoomsException;
 import se.grupp3.hotellbokningssystem.model.Booking;
 import se.grupp3.hotellbokningssystem.service.BookingService;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -25,13 +26,13 @@ public class BookingController {
     private BookingService bookingService;
 
     @GetMapping("/bookings")
-    public ResponseEntity<?> getBookings() {
-        return ResponseEntity.ok(bookingService.getBookings().stream().map(BookingResponse::new));
+    public ResponseEntity<?> getBookings(Authentication authentication) {
+        return ResponseEntity.ok(bookingService.getBookings(authentication).stream().map(BookingResponse::new));
     }
 
     @PostMapping("/bookings")
-    public ResponseEntity<?> createBooking(@Valid @RequestBody BookingRequest bookingRequest) throws Exception {
-        Booking b = bookingService.createBooking(bookingRequest.getGuestName(), bookingRequest.getNights(), bookingRequest.getGuestCount(), bookingRequest.getRoomType());
+    public ResponseEntity<?> createBooking(Authentication authentication, @Valid @RequestBody BookingRequest bookingRequest) throws Exception {
+        Booking b = bookingService.createBooking(authentication, bookingRequest.getGuestName(), bookingRequest.getNights(), bookingRequest.getGuestCount(), bookingRequest.getRoomType());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(new BookingResponse(b));
     }
